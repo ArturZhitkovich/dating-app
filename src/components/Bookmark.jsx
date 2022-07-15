da088003bc46f1cb30761a0a93d376cf4ec6d20f
Artur Zhitkovich <artur.zhitkovich15041998@gmail.com>
2022-07-15 14:55:42 +0300

    Update Bookmark.jsx

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Bookmark = ({status, onToggleBookMark, id}) => {
+    // смотри, ты сделал комопнент, и чтобы его правильно использовать, ты должен говорить каждому разработчику, что тебе надо писать дополнительную логику на обработку 
+    
     return (
         <i
             onClick={() => onToggleBookMark(id)}
